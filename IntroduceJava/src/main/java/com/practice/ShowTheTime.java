135ad15fbe65d4433256db2df001a8f3c0a6b5dd
ThienJ97 <tieuquyy1002@gmail.com>
2023-03-09 15:41:38 +0700

    ShowTheTime

---
+++ IntroduceJava/src/main/java/com/practice/ShowTheTime.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+import java.util.Date;
+
+public class ShowTheTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
