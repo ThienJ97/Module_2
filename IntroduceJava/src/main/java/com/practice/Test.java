2cb2faadc81ec77324410494457d2299fced1a34
ThienJ97 <124329303+ThienJ97@users.noreply.github.com>
2023-03-14 08:22:09 +0700

    Delete Test.java

--- IntroduceJava/src/main/java/com/practice/Test.java
+++
@@ -1,9 +0,0 @@
-package com.practice;
-import java.sql.Date;
-import java.util.Locale;
-public class Test {
-    public static void main(String[] args) {
-        Date data = new Date(0);
-        System.out.printf(Locale.GERMANY, "Ngày giờ hiện tại: %tc\n", data);
-    }
-}
